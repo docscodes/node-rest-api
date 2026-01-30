de473333497bad42cabe7cfbee9e69ad9175c70a
docscodes <docscodes@gmail.com>
2026-01-30 13:15:59 -0500

    update a post

--- routes/posts.js
+++ routes/posts.js
@@ -15,6 +15,21 @@ router.post("/", async (req, res) => {
 });
 
 //update a post
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+
+      res.status(200).json("The post has been updated");
+    } else {
+      res.status(403).json("You can only update your own post.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //delete a post
 

2786b529e6425115f127181f01c4e683d8c3609d
docscodes <docscodes@gmail.com>
2026-01-30 13:30:35 -0500

    delete a post

--- routes/posts.js
+++ routes/posts.js
@@ -32,6 +32,21 @@ router.put("/:id", async (req, res) => {
 });
 
 //delete a post
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+
+      res.status(200).json("The post has been deleted.");
+    } else {
+      res.status(403).json("You can only delete your own post.");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //like / dislike a post
 
